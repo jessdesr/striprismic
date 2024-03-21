b164a00c76ecf1155a170958512003cdb1093603
Jess Desrochers <shaderone.jd@gmail.com>
2024-03-21 16:42:17 -0700

    first commit

---
+++ src/slices/Hero/index.tsx
@@ -0,0 +1,32 @@
+import { RichText } from "@/components/RichText";
+import { Content } from "@prismicio/client";
+import { PrismicNextImage } from "@prismicio/next";
+import { PrismicText, SliceComponentProps } from "@prismicio/react";
+
+/**
+ * Props for `Hero`.
+ */
+export type HeroProps = SliceComponentProps<Content.HeroSlice>;
+
+/**
+ * Component for "Hero" Slices.
+ */
+const Hero = ({ slice }: HeroProps): JSX.Element => {
+  return (
+    <section
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+      className="flex flex-col gap-4 max-w-3xl w-full"
+    >
+      <PrismicNextImage field={slice.primary.image} sizes="100vw" className="w-full max-w-[100px] max-h-full rounded-md object-cover" />
+      <div>
+        <h1>
+          <PrismicText field={slice.primary.title} />
+        </h1>
+        <RichText field={slice.primary.description} />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
